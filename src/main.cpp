798321b4429524c2c1c1de549a16d8e9eefa5981
Laszlo C Bardos <laszlo@bardos.us>
2021-07-23 12:09:14 -0400

    Final code

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,33 @@
-//Program to randomly shuffle the names of students in a class and display a different student name
+//Program to shuffle the names of students in a class and display a different student name
 //every time a button is pressed. Display is a 4 x 8x8 Dot matrix panel controlled by MAX7219
-#include "Arduino.h"
+#include <Arduino.h>
 #include <MD_Parola.h>  //Library of nice functions for the display
 #include <MD_MAX72xx.h> //Driver for display
 #include <SPI.h>
 #include "LowPower.h"
-#define CLASS_PIN 3 //pin for the pushbutton to change classes
 #define NEXT_PIN 2 //Pin to get next student
+#define CLASS_PIN 3 //pin for the pushbutton to change classes
 #define CS_PIN 4  //control pin for the display
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4  //Number of 8x8 LED matrices
 #define DISPLAY_SCROLL_SPEED  50 //msec between frames
 #define DISPLAY_SCROLL_PAUSE_TIME  0
+#define DISPLAY_BRIGHTNESS 5 // The intensity (brightness) of the display (0-15):
 MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES); //new instance of MD_Parola class with hardware SPI
 
 //Edit these variables to match your situation.
-const int maxClassSize = 12;  //Enter the number of students in the largest class
+const int maxClassSize = 9;  //Enter the number of students in the largest class
 const int numberOfClasses = 3; //How many classes
-const int numberOfStudents[] = {12, 10, 6}; //Enter the number of students in each class
-const char *classNames[] = {"Geo A", "Geo D", "C-Alg"}; //Abbreviate to < 8 characters to fit on display
+const int numberOfStudents[] = {9, 7, 8}; //Enter the number of students in each class
+const char *classNames[] = {"Geo", "Calc C", "Calc D"}; //Abbreviate to < 8 characters to fit on display
 const char *classRosters[numberOfClasses][maxClassSize] = {
-            {"Angie", "Jill", "Mairi", "Allie", "Jacey", "Jennifer", "Jojo", "Nozomi",
-              "Lillian", "Erica", "Mehar", "Ada"},
-            {"Ashlyn", "Guialem", "Sydney", "Sadie", "Raji", "Annie", "Tess", "Annika",
-              "Olana", "Claire"}, 
-            {"Morgan", "Grace", "Sallie", "Esme", "Sophie", "Bella"},
+            {"Amy", "Bob", "Chris", "Dab", "Elle", "Fran", "Greg", "Hank","Inigo"},
+            {"Micky", "Daffy", "Minnie", "Bugs", "Sam", "Max", "Jessica"}, 
+            {"Al", "Bea", "Cal", "Doug", "Emma", "Fred", "Gail", "Hal"}
           };
 
 void shuffleStudents(); //declare functions
+void startDisplay();
 void changeClass();
 void getNextStudent();
 void wakeUp();
@@ -39,18 +39,15 @@ int nextPinUp = false;
 int classPinUp = false;
 
 void setup() {
-  //Serial.begin(9600);
+  
   pinMode(NEXT_PIN, INPUT_PULLUP); //The pushbutton pin will have an internal resistor pulling the value high when not depressed
   pinMode(CLASS_PIN, INPUT_PULLUP);
   pinMode(CS_PIN, OUTPUT);
-  
-  randomSeed(analogRead(0)); //get noise from pin 0 as seed for random number generator
-
-  myDisplay.begin();  //Start the display module
-  myDisplay.setIntensity(7);  // Set the intensity (brightness) of the display (0-15):
-  myDisplay.setTextAlignment(PA_CENTER);
+  startDisplay();
+  myDisplay.print("Derby"); //display my silly name for the random caller. "I didn't call on you. Derby did!"
+  while (digitalRead(CLASS_PIN)){} //wait for the button to be pressed so I can get a random time.
+  randomSeed(millis());  //initialize my random number generator
   myDisplay.displayClear();
-  myDisplay.displayReset();
   changeClass(); //go to the next class and display its name.
 }
 
@@ -73,6 +70,14 @@ void loop() {
   }
 }
 
+void startDisplay(){
+  myDisplay.begin();
+  myDisplay.setIntensity(DISPLAY_BRIGHTNESS);  
+  myDisplay.setTextAlignment(PA_CENTER);
+  myDisplay.displayClear();
+  myDisplay.displayReset();
+  myDisplay.displayShutdown(false);
+}
 void changeClass() {
   thisClass = (thisClass + 1) % numberOfClasses;
   shuffleStudents();
@@ -93,19 +98,13 @@ void shuffleStudents() {
     shuffledIndexes[i] = shuffledIndexes[j];
     shuffledIndexes[j] = t;
   } 
-  /*  for (int i = 0; i < numberOfStudents[thisClass]; i++) {
-      Serial.print(classRosters[thisClass][shuffledIndexes[i]]);
-      Serial.print(" ");
-     Serial.println(strlen(classRosters[thisClass][shuffledIndexes[i]]));
-  }
-  */ 
 }  
 
 void getNextStudent() {
   myDisplay.displayShutdown(false); //wake up display from low power mode
   thisStudent = (thisStudent + 1) % numberOfStudents[thisClass];
   int del = 0;
-  for (int i = 0; i < 40; i++) {
+  for (int i = 0; i < 30; i++) {
     int student = (thisStudent + i) % numberOfStudents[thisClass];
     myDisplay.print(classRosters[thisClass][student]);
     del = del + i/4;
